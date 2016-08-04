5d8b935d1d66d9dc982b16e2866b3c46b652ef0e
Ryan Joy <atxryan@gmail.com>
2016-08-04 13:33:42 -0500

    Add nconf dependency

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+var restify = require('restify'),
+    builder = require('botbuilder'),
+    nconf   = require('nconf');
+
+// Create nconf environment to load keys and connections strings
+// which should not end up on GitHub
+    nconf 
+        .file({ file: './config.json' }) 
+        .env(); 
 
 //=========================================================
 // Bot Setup
@@ -13,8 +20,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: nconf.get("MICROSOFT_APP_ID"),
+    appPassword: nconf.get("MICROSOFT_APP_PASSWORD")
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
