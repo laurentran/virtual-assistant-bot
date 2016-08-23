d74fabd33eb80843f3c1479fc54e46db65fb673b
Ryan Joy <atxryan@gmail.com>
2016-08-22 22:22:31 -0500

    Add LUIS recognizer, intents, and static index

--- app.js
+++ app.js
@@ -23,13 +23,40 @@ var connector = new builder.ChatConnector({
     appId: nconf.get("MICROSOFT_APP_ID"),
     appPassword: nconf.get("MICROSOFT_APP_PASSWORD")
 });
+
 var bot = new builder.UniversalBot(connector);
+
 server.post('/api/messages', connector.listen());
+server.get(/.*/, restify.serveStatic({
+    'directory' : '.',
+    'default'   : 'index.html'
+}));
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+// Create LUIS recognizer that points at our model
+var model = nconf.get("LUIS_model_URL");
+var recognizer = new builder.LuisRecognizer(model);
+var intents = new builder.IzntentDialog({ recognizers: [recognizer] });
+
+bot.dialog('/', intents);
+
+intents
+    .onBegin(builder.DialogAction.send("Hi, I'm your startup assistant!"))
+    // Simple regex commands
+    .matches(/^hello/i, function (session) {
+        session.send("Hi there!");
+    })
+    .matches(/^help/i, function (session) {
+        session.send("You asked for help.");
+    })
+    //LUIS intent matches
+    .matches('AzureCompliance', '/compliance')
+    //.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand, but I'm learning."));
+
+bot.dialog('/compliance', function (session, args) {
+    session.send("You asked about Azure Compliance.");
+    session.endDialog();
 });
